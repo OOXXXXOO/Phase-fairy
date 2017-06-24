10e5d85445f8f21d9e080e3e93fd98881b7983be
Tom Winshare <785929784@qq.com>
2017-06-24 13:45:41 +0800

    first commit

---
+++ Assets/walle/tag_destroy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tag_destroy : MonoBehaviour {
+    private float time = 0f;
+    public float diren_lifetime = 10;
+    public void destroy_tag()
+    {
+        gameObject.SetActive(false);
+        DestroyImmediate(this.gameObject);
+    }
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time>diren_lifetime)
+        {
+            destroy_tag();
+        }
+    }
+}
